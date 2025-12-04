30cb2b7ba2de33f6a1455201cd6b1bab9ff47424
SamuelCastrillon <samcastj@gmail.com>
2025-12-04 18:37:07 -0500

    Add initial project files including .gitignore, solution, and core engine setup

---
+++ CoreEngine/Program.cs
@@ -0,0 +1,12 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace SmallEngineCS
+{
+    public class Game : GameWindow
+    {
+         
+    }
+}
