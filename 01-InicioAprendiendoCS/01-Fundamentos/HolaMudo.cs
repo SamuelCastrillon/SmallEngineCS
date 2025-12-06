c171351ffe70a5fa2d8dbbbc3b7c3675cf5aacc6
SamuelCastrillon <samcastj@gmail.com>
2025-12-06 13:55:22 -0500

    Add HolaMudo.cs with initial console output examples

---
+++ 01-InicioAprendiendoCS/01-Fundamentos/HolaMudo.cs
@@ -0,0 +1,23 @@
+// Console.WriteLine("Hello, World");
+
+// Console.WriteLine("This is the first line.");
+// Console.Write("This is the second line.");
+
+// Console.WriteLine('b');
+
+// Console.WriteLine(123);
+
+// Float Type    Precision
+// ----------------------------
+// float         ~6-9 digits
+// double        ~15-17 digits
+// decimal        28-29 digits
+
+// Console.WriteLine(0.25F);
+
+// Console.WriteLine(2.625);
+
+// Console.WriteLine(12.39816m);
+
+Console.WriteLine(true);
+Console.WriteLine(false);
